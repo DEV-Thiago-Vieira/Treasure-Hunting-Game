13fd522f5437804c0785dc027cbf4bc52adc2275
Thiago Vieira <pessoalvieira779@gmail.com>
2026-04-17 19:46:09 -0300

    feat: FPS and character base movement system added

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 
     final int originalTileSize = 16;
     final int scale = 3;
@@ -13,9 +13,75 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    KeyHandler keyHandler = new KeyHandler();
+    Thread gameThread;
+
+    int FPS = 60;
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
+    }
+
+    public void startGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000 / FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null) {
+            update();
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime /= 1000000;
+                if(remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void update() {
+        if(keyHandler.upPressed) {
+            playerY -= playerSpeed;
+        }
+
+        else if(keyHandler.downPressed) {
+            playerY += playerSpeed;
+        }
+
+        else if(keyHandler.leftPressed) {
+            playerX -= playerSpeed;
+        }
+
+        else if(keyHandler.rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
     }
 }
