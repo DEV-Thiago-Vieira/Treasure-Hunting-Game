b69bc4d39ae81c17966c11f4762df054234289c0
Thiago Vieira <pessoalvieira779@gmail.com>
2026-04-21 20:15:47 -0300

    feat: player sprites animation applied

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -5,6 +5,7 @@ import Main.KeyHandler;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class Player extends Entity {
@@ -41,25 +42,63 @@ public class Player extends Entity {
         }
     }
     public void update() {
-        if(keyHandler.isUpPressed()) {
-            y -= speed;
-        }
 
-        else if(keyHandler.isDownPressed()) {
-            y += speed;
-        }
+        if(keyHandler.isUpPressed() || keyHandler.isDownPressed() || keyHandler.isLeftPressed() || keyHandler.isRightPressed()) {
+            if(keyHandler.isUpPressed()) {
+                direction = "up";
+                y -= speed;
+            }
 
-        else if(keyHandler.isLeftPressed()) {
-            x -= speed;
-        }
+            else if(keyHandler.isDownPressed()) {
+                direction = "down";
+                y += speed;
+            }
 
-        else if(keyHandler.isRightPressed()) {
-            x += speed;
+            else if(keyHandler.isLeftPressed()) {
+                direction = "left";
+                x -= speed;
+            }
+
+            else if(keyHandler.isRightPressed()) {
+                direction = "right";
+                x += speed;
+            }
+
+            spriteCounter++;
+
+            if(spriteCounter > 12) {
+                if(spriteNum == 1) {
+                    spriteNum = 2;
+                }else if(spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
         }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
+//        g2.setColor(Color.white);
+//        g2.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
+        BufferedImage image = null;
+        switch (direction) {
+            case "up":
+                if(spriteNum == 1) image = up1;
+                if(spriteNum == 2) image = up2;
+                break;
+            case "down":
+                if(spriteNum == 1) image = down1;
+                if(spriteNum == 2) image = down2;
+                break;
+            case "left":
+                if(spriteNum == 1) image = left1;
+                if(spriteNum == 2) image = left2;
+                break;
+            case "right":
+                if(spriteNum == 1) image = right1;
+                if(spriteNum == 2) image = right2;
+                break;
+        }
+        g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
     }
 }
