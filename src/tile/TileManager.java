5320bd3c32a7a9274e6a844d0584a6a0baf32204
Thiago Vieira <pessoalvieira779@gmail.com>
2026-05-09 07:56:37 -0300

    feat: rendering improved

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -80,12 +80,21 @@ public class TileManager {
 
         while (worldCol < gamePanel.maxWorldCol && worldRow < gamePanel.maxWorldRow) {
             int tileNum = mapTileNum[worldCol][worldRow];
+
             int worldX = worldCol * gamePanel.tileSize;
             int worldY = worldRow * gamePanel.tileSize;
             int screenX = worldX - gamePanel.player.worldX + gamePanel.player.screenX;
             int screenY = worldY - gamePanel.player.worldY + gamePanel.player.screenY;
 
-            g2.drawImage(tile[tileNum].image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
+            if(
+                    worldX + gamePanel.tileSize > gamePanel.player.worldX - gamePanel.player.screenX &&
+                    worldX - gamePanel.tileSize < gamePanel.player.worldX + gamePanel.player.screenX &&
+                    worldY + gamePanel.tileSize > gamePanel.player.worldY - gamePanel.player.screenY &&
+                    worldY - gamePanel.tileSize < gamePanel.player.worldY + gamePanel.player.screenY
+            ) {
+                g2.drawImage(tile[tileNum].image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
+            }
+
             worldCol++;
 
             if(worldCol == gamePanel.maxWorldCol) {
